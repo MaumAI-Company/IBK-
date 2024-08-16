3b7b3194c67d6a9c31f0f72275b28376d21ca4e9
yeonjin.lee <yeonjin.lee@ntoday.kr>
2024-08-16 09:32:17 +0900

    안쓰는 라이브러리 정리

--- src/main/resources/static/ClientUI/js/style.js
+++ src/main/resources/static/ClientUI/js/style.js
@@ -3,16 +3,6 @@ $(document).ready(function(){
 	$('.btn_toggle_aside').on('click', function(){
 		$('body').toggleClass('aside_hide');
 	});
-	$('#aside nav').mouseenter(function () {
-		$(this).addClass('hover');
-	}).mouseleave(function () {
-		$(this).removeClass('hover');
-	});
-	$('#aside nav').focusin(function () {
-		$(this).addClass('hover');
-	}).focusout(function () {
-		$(this).removeClass('hover');
-	});
 
 	//lnb
 	$('#aside .nav li').each(function(){
@@ -22,6 +12,7 @@ $(document).ready(function(){
 
 		$(this).children('a').on('click', function(){
 			if($(this).not(':only-child')){
+				$('body').removeClass('aside_hide');
 				$(this).next('ul').stop().slideToggle().parent('li').toggleClass('active').siblings().removeClass('active').children('ul').stop().slideUp();
 			}
 		});
@@ -51,62 +42,14 @@ $(document).ready(function(){
 		}
 	});
 
-	//상단으로 스크롤
-	$('.btn_top').on('click', function(){
-		$('html, body').stop().animate({'scrollTop':0},500);
-	});
-
 	//input
 	inputActive();
 
 	//전체 선택
 	checkAll();
 
-	//기간 선택 버튼
-	var d = new Date(), week = new Date(), month = new Date(), threeMonth = new Date();
-	$('.term_form').each(function(){
-		var termForm = $(this);
-		termForm.find('.btn').each(function(){
-			var btn = $(this);
-			btn.on('click', function(){
-				termForm.children('.datepicker').eq(1).val(d.toLocaleDateString('fr-CA'));
-				if(btn.hasClass('btn_today')){ //오늘
-					termForm.children('.datepicker').eq(0).val(d.toLocaleDateString('fr-CA'));
-				}else if(btn.hasClass('btn_1week')){ //1주일
-					week.setDate(week.getDate() - 7);
-					termForm.children('.datepicker').eq(0).val(week.toLocaleDateString('fr-CA'));
-				}else if(btn.hasClass('btn_1month')){ //최근 1개월
-					month.setMonth(month.getMonth() - 1);
-					termForm.children('.datepicker').eq(0).val(month.toLocaleDateString('fr-CA'));
-				}else if(btn.hasClass('btn_3month')){ //최근 3개월
-					threeMonth.setMonth(threeMonth.getMonth() - 3);
-					termForm.children('.datepicker').eq(0).val(threeMonth.toLocaleDateString('fr-CA'));
-				}
-			});
-		});
-	});
-
 	//select
 	niceSelect();
-
-	//textarea
-	$('textarea').each(function () {
-		$(this).on('focusin', function() {
-			$(this).parents('.scroll-textarea').addClass('active');
-		}).on('focusout', function() {
-			$(this).parents('.scroll-textarea').removeClass('active');
-		});
-	});
-
-	//tab
-	$('.tab > li').each(function () {
-		var tabLi = $(this),
-			tabLiIdx = tabLi.index();
-		tabLi.find('a').on('click', function () {
-			tabLi.addClass('active').siblings().removeClass('active');
-			tabLi.parent('.tab').siblings('.tab_cont').children('.cont').eq(tabLiIdx).addClass('active').siblings().removeClass('active');
-		});
-	});
 
 	//Snackbar 닫기
 	$('.snackbar').each(function () {
@@ -122,16 +65,6 @@ $(document).ready(function(){
 			$(this).parent('.pop_layer').removeClass('active');
 			$('html,body').removeAttr('style');
 		});
-	});
-
-	resize(); scroll();
-
-	$(window).scroll(function(){
-		scroll();
-	});
-
-	$(window).resize(function(){
-		resize();
 	});
 });
 
@@ -234,37 +167,6 @@ function inputActive(){
 			divInp.removeClass('active').find('input').val('');
 		});
 	});
-
-	//input number comma
-	$('.comma').inputmask('numeric', {
-		autoGroup: true,
-		groupSeparator: ',',
-	});
-	$('.comma').keyup(function(){
-		var commaNum = $(this).prop('value');
-		if(commaNum.match('-')){
-			$(this).prop('value','');
-		}
-	});
-
-	//input datepicker form
-	/*$('.datepicker').inputmask({
-		mask: '9999.99.99',
-		showMaskOnHover: false,
-		showMaskOnFocus: false,
-	});*/
-
-	//input 한글만
-	$(document).on("keyup", "input.only_kor", function() {
-		var pattern = /[a-z0-9]|[ \[\]{}()<>?|`~!@#$%^&*-_+=,.;:\"'\\]/g;
-		this.value = this.value.replace(pattern, '');
-	});
-
-	//input 영어만
-	$(document).on("keyup", "input.only_eng", function() {
-		var pattern =  /[^A-Za-z\s]/ig;
-		this.value = this.value.replace(pattern, '');
-	});
 }
 
 //checkbox all
@@ -296,26 +198,6 @@ function checkAll(){
 			}
 		});
 	});
-}
-
-function scroll(){
-	var sct = $(window).scrollTop();
-
-	if(sct > 0){
-		$('#header').addClass('fixed');
-	}else{
-		$('#header').removeClass('fixed');
-	}
-}
-
-function resize(){
-	var winW = $(window).outerWidth(),
-		winH = $(window).outerHeight();
-
-	if(winW >= 1024){ //pc
-	}else{ //tablet & mobile
-	}
-
 }
 
 //개발용 common js 에 있는 소스(실 작업시 중복될 수 있음) : s
@@ -358,8 +240,8 @@ function maxLengthCheck(object){
 
 //레이어 팝업 열기
 function layerShow(ele,obj){
-	var winH = $(window).height(),
-		popCont = $('#'+ele).not('.system_alert').find('.pop_cont').not('.noscroll');
+	var winH = $(window).height();
+
 	$('html, body').css({'overflow':'hidden'});
 	$('#'+ele).addClass('active');
 
@@ -370,17 +252,6 @@ function layerShow(ele,obj){
 		$(obj).attr('data-focus','alert');
 	}
 	$('#'+ele).attr('tabindex','0').focus();
-
-	//팝업 열때 콘텐츠 스크롤바 추가
-	/*if(popCont.parent('div').hasClass('w1280')){
-		popCont.scrollbar();
-	}*/
-
-	popCont.find('.tbl_scrollbar').each(function () {
-		if(!$(this).hasClass('noscroll')){
-			$(this).scrollbar();
-		}
-	});
 }
 //레이어 팝업 닫기
 function layerHide(ele){
@@ -404,13 +275,6 @@ function layerHide(ele){
 	$('#'+ele).removeAttr('tabindex','0');
 }
 
-//윈도우 팝업
-function winPop(url, title, w, h){
-	var top = (screen.height/2)-(h/2),
-		left = (screen.width/2)-(w/2);
-	return window.open(url, title, 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width='+w+', height='+h+', top='+top+', left='+left);
-}
-
 //Toast & Snackbar
 function systemMsg(ele){
 	$('#'+ele).addClass('active');
@@ -422,23 +286,4 @@ function systemMsg(ele){
 			clearTimeout(toast);
 		}
 	}, 3000);
-}
-
-function  fn_setDatePickerTime(selector) {
-	$(selector).daterangepicker({
-		"singleDatePicker": true,
-		"timePicker": true,
-		"timePickerIncrement": 10,
-		"locale": {
-			format: 'YYYY-MM-DD HH:mm:ss',
-			separator: ' ~ ',
-			applyLabel: '확인',
-			cancelLabel: '닫기',
-			daysOfWeek: ['일', '월', '화', '수', '목', '금', '토'],
-			monthNames: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
-			firstDay: 1,
-		},
-		"drops": "auto",
-		"timePickerIncrement": 1,
-	});
 }
