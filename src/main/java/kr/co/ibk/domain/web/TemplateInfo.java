c6f2e88ec06ac2f9b5ebdc2f678316f6bc7401a9
chaerin.song <chaerin.song@ntoday.kr>
2024-11-20 14:48:09 +0900

    [IBK] 완료 새테이블 model ~ repository 생성

---
+++ src/main/java/kr/co/ibk/domain/web/TemplateInfo.java
@@ -0,0 +1,25 @@
+package kr.co.ibk.domain.web;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class TemplateInfo {
+    private String id;
+    private String templateName; /*템플릿명*/
+    private String selectCon; /*선택조건*/
+    private String hdqrBobDcd; /*1 본부, 2 영업점*/
+    private String regId; /*등록자*/
+    private LocalDateTime regDt; /*등록일자*/
+    private String modId; /*수정자*/
+    private LocalDateTime modDt; /*수정일자*/
+}
+
+
