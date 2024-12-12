084d34d787d165cb31f5d485251b618e80577cbc
chaerin.song <chaerin.song@ntoday.kr>
2024-12-12 15:14:39 +0900

    [IBK] 완료 BC카드 지급결의 > 상세보기 : output 데이터가 없는 경우에도 input 데이터 열람 가능하도록 수정 https://itsm.ntoday.kr/ITSM-045-05393

--- src/main/java/kr/co/ibk/service/CardOutputService.java
+++ src/main/java/kr/co/ibk/service/CardOutputService.java
@@ -1,6 +1,7 @@
 package kr.co.ibk.service;
 
 import kr.co.ibk.domain.enums.InOutGbnType;
+import kr.co.ibk.domain.enums.InputColumnType;
 import kr.co.ibk.domain.web.CardInputInfo;
 import kr.co.ibk.domain.web.CardOutputInfo;
 import kr.co.ibk.domain.web.LearningModelInputInfo;
@@ -14,6 +15,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.ObjectUtils;
 
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -28,34 +30,46 @@ public class CardOutputService extends _BaseService {
     public CardOutputInfo detail(CardInputForm params) {
         CardOutputInfo info = cardOutputRepository.getDetail(params);
 
-        if (!ObjectUtils.isEmpty(info) && !ObjectUtils.isEmpty(info.getLearningModelId())) {
-            List<LearningModelInputInfo> inputInfos = learningModelInputRepository.getPartList(info.getLearningModelId(), InOutGbnType.INPUT.name());
-            if (!ObjectUtils.isEmpty(inputInfos)) {
-                inputInfos.forEach(input -> {
-                    input.setInputColumnNm(input.getInputColumnType().getName());
+        List<LearningModelInputInfo> inputInfos = new ArrayList<>();
 
-                    String columnName = input.getInputColumnType().getCamelColumn();
+        if (!ObjectUtils.isEmpty(info.getLearningModelId())) {
+            inputInfos = learningModelInputRepository.getPartList(info.getLearningModelId(), InOutGbnType.INPUT.name());
+        }
+        if (ObjectUtils.isEmpty(inputInfos)) {
+            List<LearningModelInputInfo> defaultInputList = new ArrayList<>();
 
-                    // info 객체의 해당 메서드 호출
-                    try {
-                        // 메서드 이름 생성
-                        String getterMethodName = "get" + columnName.substring(0, 1).toUpperCase() + columnName.substring(1);
-                        Method method = info.getClass().getMethod(getterMethodName);
-
-                        // 메서드 호출하여 값 가져오기
-                        Object columnValue = method.invoke(info);
-                        input.setInputColumnVal(columnValue != null ? columnValue.toString() : null);
-
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                });
-                info.setInputList(inputInfos);
+            for (InputColumnType value : InputColumnType.values()) {
+                LearningModelInputInfo defaultInput = new LearningModelInputInfo();
+                defaultInput.setInputColumnType(value);
+                defaultInputList.add(defaultInput);
             }
+            inputInfos = defaultInputList;
         }
 
+        inputInfos.forEach(input -> {
+            input.setInputColumnNm(input.getInputColumnType().getName());
+
+            String columnName = input.getInputColumnType().getCamelColumn();
+
+            // info 객체의 해당 메서드 호출
+            try {
+                // 메서드 이름 생성
+                String getterMethodName = "get" + columnName.substring(0, 1).toUpperCase() + columnName.substring(1);
+                Method method = info.getClass().getMethod(getterMethodName);
+
+                // 메서드 호출하여 값 가져오기
+                Object columnValue = method.invoke(info);
+                input.setInputColumnVal(columnValue != null ? columnValue.toString() : null);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+        info.setInputList(inputInfos);
+
+
         //이전/다음글 no find
-        for (int i = 0; i < 2; i++) {
+        /*for (int i = 0; i < 2; i++) {
             params.setLoadType(i == 0 ? "prev" : "next");
 
             if (ObjectUtils.isEmpty(params.getSorting())) {
@@ -74,7 +88,7 @@ public class CardOutputService extends _BaseService {
                     info.setNextNo(prevAndNext.getNo());
                 }
             }
-        }
+        }*/
 
         return info;
     }
