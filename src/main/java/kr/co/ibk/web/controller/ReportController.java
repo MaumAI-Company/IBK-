53857ba5cc603134778bbf7788bfe5aa56a4c2bb
jeonghyun.park <jeonghyun.park@ntoday.kr>
2025-01-03 14:46:50 +0900

    [IBK] 완료 결과 조회 > BC카드 지급결의 내역 조회 : 엑셀 정렬 적용

--- src/main/java/kr/co/ibk/web/controller/ReportController.java
+++ src/main/java/kr/co/ibk/web/controller/ReportController.java
@@ -56,6 +56,7 @@ public class ReportController extends BaseCont {
                                     @RequestParam(name = "searchEndDate", required = false) String searchEndDate,
                                     @RequestParam(name = "searchTarget", required = false) String searchTarget,
                                     @RequestParam(name = "searchTypeJson", required = false) String searchTypeJson,
+                                    @RequestParam(name = "sorting", required = false) String sorting,
                                     HttpServletResponse response) throws UnsupportedEncodingException {
 
         CardInputForm params = new CardInputForm();
@@ -67,6 +68,9 @@ public class ReportController extends BaseCont {
             params.setSearchJsonMap(jsonToHashMap(searchTypeJson));
         }
 
+        if (!ObjectUtils.isEmpty(sorting)) {
+            params.setSorting(sorting);
+        }
         List<CardInputInfo> excelList = cardInputService.list(params);
         cardInputService.reportCardExcelDown(response, excelList);
     }
