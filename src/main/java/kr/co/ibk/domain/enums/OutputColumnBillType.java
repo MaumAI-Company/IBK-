2d35316be48d06a1dad13bb951046beade25fc61
jeonghyun.park <jeonghyun.park@ntoday.kr>
2025-01-13 10:02:26 +0900

    [IBK] 완료 학습관리 : output column 필드명 변경 https://itsm.ntoday.kr/ITSM-045-05557

--- src/main/java/kr/co/ibk/domain/enums/OutputColumnBillType.java
+++ src/main/java/kr/co/ibk/domain/enums/OutputColumnBillType.java
@@ -2,7 +2,7 @@ package kr.co.ibk.domain.enums;
 
 public enum OutputColumnBillType {
     BDGT_ITEX_FRCS_CON("예산관리비목번호", "bdgtItexFrcsCon"),
-    BDGT_PRFR_RSN_FRCS_CON("예산관리비목관리번호_예산집행사유코드", "bdgtPrfrRsnFrcsCon"),
+    BDGT_PRFR_RSN_FRCS_CON("예산집행사유코드", "bdgtPrfrRsnFrcsCon"), //예산관리비목관리번호_예산집행사유코드
     BDGT_BSNS_FRCS_CON("사업세부사업", "bdgtBsnsFrcsCon");
 
     final private String name;
