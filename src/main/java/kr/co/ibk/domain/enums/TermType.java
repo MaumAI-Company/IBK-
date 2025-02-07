46e393a387979277aceab3ca0805a74510115ad8
류창석 <changseok.lyu@ntoday.kr>
2025-02-07 12:39:10 +0900

    주기 값 추가.

--- src/main/java/kr/co/ibk/domain/enums/TermType.java
+++ src/main/java/kr/co/ibk/domain/enums/TermType.java
@@ -1,19 +1,25 @@
 package kr.co.ibk.domain.enums;
 
 public enum TermType {
-    HALF("반기"),
-    QUARTER("분기"),
-    MONTHLY("매월"),
-    WEEKLY("매주");
+    HALF("반기", 24),
+    QUARTER("분기", 12),
+    MONTHLY("매월", 4),
+    WEEKLY("매주", 1);
 
     final private String name;
+    final private Integer week;
 
     public String getName() {
         return name;
     }
 
-    private TermType(String name) {
+    public Integer getWeek() {
+        return week;
+    }
+
+    private TermType(String name, Integer week) {
         this.name = name;
+        this.week = week;
     }
 
 }
