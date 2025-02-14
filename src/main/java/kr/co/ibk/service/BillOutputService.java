9e0baa26128146c2735244d073b16c264780833e
jeonghyun.park <jeonghyun.park@ntoday.kr>
2025-02-14 16:20:34 +0900

    [IBK] 완료 결의내역 > 상세 : 추론에 사용하지 않는 필드 대시처리

--- src/main/java/kr/co/ibk/service/BillOutputService.java
+++ src/main/java/kr/co/ibk/service/BillOutputService.java
@@ -1,17 +1,23 @@
 package kr.co.ibk.service;
 
+import kr.co.ibk.domain.enums.InOutGbnType;
 import kr.co.ibk.domain.enums.InputColumnBillType;
+import kr.co.ibk.domain.enums.LearningType;
 import kr.co.ibk.domain.web.BillOutputInfo;
 import kr.co.ibk.domain.web.LearningModelInputInfo;
 import kr.co.ibk.model.BillInputForm;
 import kr.co.ibk.repository.BillOutputRepository;
+import kr.co.ibk.repository.LearningModelInputRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.ObjectUtils;
 
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -19,37 +25,43 @@ import java.util.List;
 public class BillOutputService extends _BaseService {
 
     private final BillOutputRepository billOutputRepository;
+    private final LearningModelInputRepository learningModelInputRepository;
 
     public BillOutputInfo detail(BillInputForm params) {
         BillOutputInfo info = billOutputRepository.getDetail(params);
-
-        List<LearningModelInputInfo> defaultInputInfos = new ArrayList<>();
-        for (InputColumnBillType value : InputColumnBillType.values()) {
-            LearningModelInputInfo defaultInput = new LearningModelInputInfo();
-            defaultInput.setInputColumnBillType(value);
-            defaultInputInfos.add(defaultInput);
+        List<LearningModelInputInfo> inputInfos = new ArrayList<>();
+        if (!ObjectUtils.isEmpty(info.getLearningModelId())) {
+            inputInfos = learningModelInputRepository.getPartList(info.getLearningModelId(), InOutGbnType.INPUT.name(), LearningType.BILL.name());
         }
 
-        defaultInputInfos.forEach(input -> {
-            input.setInputColumnNm(input.getInputColumnBillType().getName());
-            String columnName = input.getInputColumnBillType().getCamelColumn();
+        Map<InputColumnBillType, LearningModelInputInfo> inputInfoMap = inputInfos.stream()
+                .collect(Collectors.toMap(LearningModelInputInfo::getInputColumnBillType, infoItem -> infoItem));
 
-            // info 객체의 해당 메서드 호출
-            try {
-                // 메서드 이름 생성
-                String getterMethodName = "get" + columnName.substring(0, 1).toUpperCase() + columnName.substring(1);
-                Method method = info.getClass().getMethod(getterMethodName);
+        List<LearningModelInputInfo> inputList = new ArrayList<>();
+        for (InputColumnBillType type : InputColumnBillType.values()) {
+            if (type != InputColumnBillType.HDQR_BOB_DCD) { // 세금계산서 상세 > 대상(본부, 영업점) 제외 (20250214 안나 매니저님 요청)
+                LearningModelInputInfo input = new LearningModelInputInfo();
+                input.setInputColumnBillType(type);
+                input.setInputColumnNm(type.getName());
 
-                // 메서드 호출하여 값 가져오기
-
-                Object columnValue = method.invoke(info);
-                input.setInputColumnVal(columnValue != null ? columnValue.toString() : null);
-
-            } catch (Exception e) {
-                e.printStackTrace();
+                if (inputInfoMap.containsKey(type)) {
+                    String columnName = type.getCamelColumn();
+                    try {
+                        String getterMethodName = "get" + columnName.substring(0, 1).toUpperCase() + columnName.substring(1);
+                        Method method = info.getClass().getMethod(getterMethodName);
+                        Object columnValue = method.invoke(info);
+                        input.setInputColumnVal(columnValue != null ? columnValue.toString() : null);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    input.setInputColumnVal("-");
+                }
+                inputList.add(input);
             }
-        });
-        info.setInputList(defaultInputInfos);
+        }
+
+        info.setInputList(inputList);
         return info;
     }
 
