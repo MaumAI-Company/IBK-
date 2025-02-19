7513ee8bf97f05344c55a71933992d7da102fe07
jeonghyun.park <jeonghyun.park@ntoday.kr>
2025-02-19 16:54:17 +0900

    [IBK] 완료 에러 페이지 추가 : https://itsm.ntoday.kr/ITSM-045-05834

--- src/main/java/kr/co/ibk/web/WebErrorCont.java
+++ src/main/java/kr/co/ibk/web/WebErrorCont.java
@@ -3,6 +3,7 @@ package kr.co.ibk.web;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.RequestDispatcher;
@@ -11,10 +12,18 @@ import javax.servlet.http.HttpServletRequest;
 @Controller
 public class WebErrorCont implements ErrorController {
 
+    private static final String ERROR_PATH = "/error";
+
+    @Override
+    public String getErrorPath() {
+        return ERROR_PATH;
+    }
+
     @GetMapping("/error")
-    public String handleError(HttpServletRequest request) {
+    public String handleError(HttpServletRequest request, Model model) {
 
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        model.addAttribute("pageTitle", "Error");
 
         if (status != null) {
             int statusCode = Integer.parseInt(status.toString());
@@ -26,8 +35,4 @@ public class WebErrorCont implements ErrorController {
         return "/soulGod/error";
     }
 
-    @Override
-    public String getErrorPath() {
-        return "/error";
-    }
 }
