0f659a33816d4725b7d66b2a755162afb0dec18f
jeonghyun.park <jeonghyun.park@ntoday.kr>
2025-08-05 16:22:24 +0900

    [IBK] 작업중 통계 배치 작업

--- src/main/java/kr/co/ibk/repository/CardOutputRepository.java
+++ src/main/java/kr/co/ibk/repository/CardOutputRepository.java
@@ -1,23 +1,12 @@
 package kr.co.ibk.repository;
 
 import kr.co.ibk.domain.web.CardOutputInfo;
-import kr.co.ibk.domain.web.UserStatusStatInfo;
 import kr.co.ibk.model.CardInputForm;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface CardOutputRepository {
     CardOutputInfo getDetail(CardInputForm form);
 
     int updateHitYn();
-
-    List<UserStatusStatInfo> getUserUsageStat();
-
-    int existsUserUsageStat(UserStatusStatInfo cardStats);
-
-    void updateUserUsageStat(UserStatusStatInfo cardStats);
-
-    void insertUserUsageStat(UserStatusStatInfo cardStats);
 }
--- src/main/java/kr/co/ibk/web/rest/TestRestController.java
+++ src/main/java/kr/co/ibk/web/rest/TestRestController.java
@@ -3,8 +3,7 @@ package kr.co.ibk.web.rest;
 import kr.co.ibk.common.ResponseDto;
 import kr.co.ibk.domain.enums.ResultCodeType;
 import kr.co.ibk.domain.web.LearningSchedulerInfo;
-import kr.co.ibk.service.CardOutputService;
-import kr.co.ibk.service.LearningSchedulerService;
+import kr.co.ibk.service.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,6 +20,9 @@ public class TestRestController {
 
     private final LearningSchedulerService learningSchedulerService;
     private final CardOutputService cardOutputService;
+    private final BillOutputService billOutputService;
+    private final UserUsageStatService userUsageStatService;
+    private final AiPrfrStatService aiPrfrStatService;
 
 
     @GetMapping("/callSchedule")
@@ -33,7 +35,14 @@ public class TestRestController {
     @GetMapping("/updateHit")
     public ResponseEntity<?> updateHit() {
         cardOutputService.updateHitYn();
+        billOutputService.updateHitYn();
+        return new ResponseEntity<>(new ResponseDto<>(ResultCodeType.SUCCESS, "성공", null), HttpStatus.OK);
+    }
 
+    @GetMapping("/updateStat")
+    public ResponseEntity<?> updateStat() {
+        userUsageStatService.updateStatistic();
+        aiPrfrStatService.updateStatistic();
         return new ResponseEntity<>(new ResponseDto<>(ResultCodeType.SUCCESS, "성공", null), HttpStatus.OK);
     }
 }
